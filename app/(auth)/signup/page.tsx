c9d7333d95be3aa3fd906a2b93958b42d1104999
Mauli Saxena <maulisaxena.16@gmail.com>
2025-12-13 13:09:32 +0530

    signup page ui

---
+++ app/(auth)/signup/page.tsx
@@ -0,0 +1,36 @@
+"use client";
+
+import { useState } from "react";
+import { Input } from "@/components/ui/input";
+import { Button } from "@/components/ui/button";
+
+export default function SignupPage() {
+    const [email,setEmail]=useState("");
+    const [password,setPassword]=useState("");
+    const [error,setError]=useState("");
+    return (
+    <form  className="max-w-sm mx-auto mt-20 space-y-4">
+      <h1 className="text-2xl font-bold">Sign Up</h1>
+
+      <Input
+        type="email"
+        placeholder="Email"
+        value={email}
+        onChange={(e) => setEmail(e.target.value)}
+      />
+
+      <Input
+        type="password"
+        placeholder="Password"
+        value={password}
+        onChange={(e) => setPassword(e.target.value)}
+      />
+
+      {error && <p className="text-red-500 text-sm">{error}</p>}
+
+      <Button type="submit" className="w-full">
+        Create Account
+      </Button>
+    </form>
+  );
+}
