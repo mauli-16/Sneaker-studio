8e17d1265bece1c2b0d745f07df380a0bd005ac9
Mauli Saxena <maulisaxena.16@gmail.com>
2025-12-14 11:19:51 +0530

    store edited for design type

--- app/store/customizerstore.ts
+++ app/store/customizerstore.ts
@@ -1,5 +1,11 @@
 import { create } from "zustand";
 
+export type Design={
+    id:string,
+    color:string,
+    text:string,
+    material:'Leather'|'Canvas'
+}
 type DesignState = {
   color: string;
   text: string;
@@ -8,8 +14,10 @@ type DesignState = {
   setColor: (color: string) => void;
   setText: (text: string) => void;
   setMaterial: (material:'Leather'|'Canvas')=>void;
+  loadDesign:(design:Design)=>void;
   reset: () => void;
 };
+
 
 export const useDesignStore = create<DesignState>((set) => ({
   color: "black",
@@ -19,6 +27,11 @@ export const useDesignStore = create<DesignState>((set) => ({
   setColor: (color) => set({color}),
   setText: (text) => set({text}),
   setMaterial:(material)=>set({material}),
+  loadDesign:(design)=>set({
+    color:design.color,
+    text:design.text,
+    material:design.material
+  }),
 
   reset: () =>
     set({
