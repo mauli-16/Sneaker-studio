c44dfcb0c5f6e0864682288e4ab2c4c2aaf50da2
Mauli Saxena <maulisaxena.16@gmail.com>
2025-12-15 01:27:16 +0530

    added test cred

--- lib/users.ts
+++ lib/users.ts
@@ -2,4 +2,9 @@ export type User={
     email:string,
     password:string
 }
-export const users:User[]=[]
+export const users: User[] = [
+    {
+        email: "test@test.com",
+        password: "$2a$10$8K1p/a0dL3LkzuLR2YaWqeRsxDVsJQ5E5Sf7P5qKJ5J8k5Q5J5J5J" // "password123"
+    }
+]
