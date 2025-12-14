59ea14217f6c05a1e5d713a0a881409b59cd9da5
Mauli Saxena <maulisaxena.16@gmail.com>
2025-12-15 00:50:10 +0530

    credentials added

--- app/(auth)/login/page.tsx
+++ app/(auth)/login/page.tsx
@@ -19,6 +19,7 @@ export default function LoginPage() {
             headers: {
                 'Content-Type': 'application/json', 
             },
+            credentials: "include",
             body: JSON.stringify({email,password}), 
         })
           if (!res.ok) {
